656728f94a95ce450f9a79f6f15d5e8f6eab4224
KarishmaKushwaha <107338389+KarishmaKushwaha@users.noreply.github.com>
2022-10-30 14:55:32 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
